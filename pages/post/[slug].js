7d16e07c496ba5fd24152f881033feeebe5cc89b
Vatsal <125674048+VatsalRathod07@users.noreply.github.com>
2023-03-31 14:56:45 +0530

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -13,6 +13,10 @@ const PostDetails = ({ post }) => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
+       <Head> 
+        <title>ZenFrost  Blog</title>
+         <link rel="icon" href="../icon.svg" />
+      </Head>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post}/>
