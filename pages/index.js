e993b95d48628530dc76b2fcbb44af4d9840133a
Vatsal <125674048+VatsalRathod07@users.noreply.github.com>
2023-03-31 14:43:14 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ posts }) {
     <div className="container mx-auto px-10 mb-8">
       <Head> 
         <title>ZenFrost  Blog</title>
-        <link rel="icon" href="/favicon.ico" />
+         <link rel="icon" href="../icon.svg" />
       </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
