58fdd22a859b7b1daaee7b38f381668e1ca0d546
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-04-24 21:32:45 +0530

    minor change in footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
@@ -7,27 +8,21 @@ function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-8 md:flex md:items-center md:justify-between border-b">
           <div className="flex justify-center md:order-2">
-            <a href="#">
+            <Link href="/">
                 <h2 className="font-signature font-semibold tracking-wider text-2xl text-ligthblack-0 text-opacity-40">UnclutteredDesk </h2>
                 {/* <p className='sub font-signature font-medium text-ligthblack-0 text-opacity-50'>Less clutter, more focus</p> */}
-            </a>
+            </Link>
           </div>
           <div className="mt-8 md:mt-0 md:order-1">
             <ul className="flex justify-center md:justify-end">
               <li className="mx-3">
-                <a href="#">
-                  <FaFacebook className="text-ligthblack-0 text-opacity-40 hover:text-ligthblack-0 h-6 w-6"/>
-                </a>
+              <Link href="https://www.instagram.com/uncluttereddesk" className="text-ligthblack-0 text-opacity-40 hover:text-opacity-100"> <FaFacebook size={25} /> </Link>
               </li>
               <li className="mx-3">
-                <a href="#">
-                  <FaTwitter className="text-ligthblack-0 text-opacity-40 hover:text-ligthblack-0 h-6 w-6" />
-                </a>
+              <Link href="https://www.instagram.com/uncluttereddesk" className="text-ligthblack-0 text-opacity-40 hover:text-opacity-100"> <FaTwitter size={25} /> </Link>
               </li>
               <li className="mx-3">
-                <a href="#">
-                  <FaInstagram className="text-ligthblack-0 text-opacity-40 hover:text-ligthblack-0 h-6 w-6" />
-                </a>
+              <Link href="https://www.instagram.com/uncluttereddesk" className="text-ligthblack-0 text-opacity-40 hover:text-opacity-100"> <FaInstagram size={25} /> </Link>
               </li>
             </ul>
           </div>
