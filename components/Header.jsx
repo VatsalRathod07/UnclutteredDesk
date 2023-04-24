44921ef08ffc69112a956c3a5e1072c8478aae89
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-04-24 10:35:52 +0530

    Changed name of Favicon

--- components/Header.jsx
+++ components/Header.jsx
@@ -20,10 +20,10 @@ const Header = () => {
     return (
       <div className="container mx-auto px-10 mb-8 ">
         <div className="w-full inline-block py-7 ">
-          <div className="md:float-left block" href="/">
+          <Link className="md:float-left block" href="/">
               <span className="text-2xl font-signature font-bold tracking-wider cursor-pointer text-ligthblack-0">UnclutteredDesk</span>
               <p className='sub font-signature font-medium capitalize text-ligthblack-0'>Less clutter, more focus</p>
-          </div>
+          </Link>
           <div className="hidden md:float-left md:contents capitalize">
             {categories.map((category) => (
               <Link key={category.slug} href={`/category/${category.slug}`}>
