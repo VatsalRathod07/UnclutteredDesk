4a1bc4fdc612aaf7d2e4c023cfd6155f7ccdeeb3
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-05-01 17:12:38 +0530

    added Analytics to the website

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'tailwindcss/tailwind.css'
 import '../style/index.scss';
 import { Layout } from '../components';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+       <Analytics/>
     </Layout>
   );
 }
